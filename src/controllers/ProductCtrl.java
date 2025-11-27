1ed86654250cdca26a36659dfbb780988101a3ce
Quitoooo <noahjj@outlook.dk>
2025-11-27 10:05:26 +0100

    ProductCtrl lavet

--- src/controllers/ProductCtrl.java
+++ src/controllers/ProductCtrl.java
@@ -1,5 +1,24 @@
 package controllers;
+import modules.Invoice;
+import modules.Alert;
+import modules.Stock;
+
 
 public class ProductCtrl {
+	private InvoiceCtrl invoiceCtrl;
+	private AlertCtrl alertCtrl;
+	
+	public ProductCtrl()	{
+	}
+
+	public Invoice insertInvoice(int invoiceNo)	{
+		invoiceCtrl.findInvoiceByNo(invoiceNo);
+	}
+	
+	public void confirmDeposit()	{
+		// Skal opdatere stock og tjekke om der skal ligges en alert
+	}
 
 }
+
+
